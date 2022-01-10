489906f9d28c381aef6a2f50701fdf8f9f50be6b
Danila Shvets <danila.schvets@gmail.com>
2022-01-10 06:27:40 +0300

    Add compatibility with previous version

--- src/iMeds/Contents/mods/iMeds/media/lua/client/Component/Configurator/EventListener/OnTickListener.lua
+++ src/iMeds/Contents/mods/iMeds/media/lua/client/Component/Configurator/EventListener/OnTickListener.lua
@@ -1,4 +1,23 @@
 local isConfigReceived = false
+
+Events.OnGameStart.Add(
+    function()
+        if SandboxVars.IsBloodSystemActive == nil then
+            SandboxVars.IsBloodSystemActive = true
+        end
+
+        if SandboxVars.IsBloodTypeSystemActive == nil then
+            SandboxVars.IsBloodTypeSystemActive = true
+        end
+
+        isConfigReceived = false
+    end
+)
+
+if not isClient() then
+    return
+end
+
 Events.OnTick.Add(
     function()
         if not isConfigReceived and getPlayer() and getPlayer():getOnlineID() then
