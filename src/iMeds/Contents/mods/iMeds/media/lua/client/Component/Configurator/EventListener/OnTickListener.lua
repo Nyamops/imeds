c2e3557d0dda8726dff9bb92b561be64f55ce557
Danila Shvets <danila.schvets@gmail.com>
2022-01-10 20:22:48 +0300

    Add configuration compatibility

---
+++ src/iMeds/Contents/mods/iMeds/media/lua/client/Component/Configurator/EventListener/OnTickListener.lua
@@ -0,0 +1,15 @@
+if not isClient() then
+    return
+end
+
+Events.OnGameStart.Add(
+    function()
+        if SandboxVars.ImmersiveMedicine.IsBloodSystemActive == nil then
+            SandboxVars.ImmersiveMedicine.IsBloodSystemActive = true
+        end
+
+        if SandboxVars.ImmersiveMedicine.IsBloodTypeSystemActive == nil then
+            SandboxVars.ImmersiveMedicine.IsBloodTypeSystemActive = true
+        end
+    end
+)
