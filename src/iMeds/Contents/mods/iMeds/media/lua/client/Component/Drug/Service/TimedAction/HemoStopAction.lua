f8853662053361235c5c353db454b825dbfdc539
Danila Shvets <danila.schvets@gmail.com>
2022-01-13 20:30:52 +0300

    Fix xp gain

--- src/iMeds/Contents/mods/iMeds/media/lua/client/Component/Drug/Service/TimedAction/HemoStopAction.lua
+++ src/iMeds/Contents/mods/iMeds/media/lua/client/Component/Drug/Service/TimedAction/HemoStopAction.lua
@@ -62,7 +62,7 @@ function HemoStopAction:perform()
     if self.character:HasTrait('Hemophobic') then
         self.character:getStats():setPanic(self.character:getStats():getPanic() + 50);
     end
-    self.character:getXp():AddXP(Perks.Doctor, 10)
+    self.character:getXp():AddXP(Perks.Doctor, 0.1)
 
     local drugApplier = InventoryItemFactory.CreateItem(DrugApplier.fullType)
     drugApplier:getModData().drug = self.drug:getModData().drug
